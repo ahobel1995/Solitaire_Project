847a35209d186b985123a01b70081b788e62a0ef
ahobel1995 <ahobel1995@gmail.com>
2023-11-15 11:34:51 -0700

    Updating Files

--- solFunc.c
+++ solFunc.c
@@ -92,9 +92,11 @@ void frameGen(int play[19][7], int hidden[19][7], int deck[53], int dropDeck[4],
     printf("\n");
     for (i = 0; i < 19; i++) {
         for (j = 0; j < 7; j++) {
+            /*
             if (hidden[i][j] == 99) {
-                //numCardPrint(hidden[i][j]);
+                numCardPrint(hidden[i][j]);
             } else {
+             */
                 numCardPrint(play[i][j]);
             }
         }
