61e21e298ddf0e0766ef5967b0a37a93f71d935e
ahobel1995 <ahobel1995@gmail.com>
2023-11-16 15:01:24 -0700

    Updating Files

--- gameLogic.c
+++ gameLogic.c
@@ -22,11 +22,11 @@ void cardMoveEval(int play[19][7], int hidden[19][7], int cardPos[2], int cardMo
         sleep(3);
         return;
         // The following check is to ensure the card is one lower, opposite suit, and not a king for a valid move.
-    } else if (play[cardPos[0]][cardPos[1]] % 13 == play[cardMovePos[0] - 1][cardMovePos[1]] % 13 - 1 &&
-            (((play[cardPos[0]][cardPos[1]] >= 1 && play[cardPos[0]][cardPos[1]] <= 13) ||
-              (play[cardPos[0]][cardPos[1]] >= 27 && play[cardPos[0]][cardPos[1]] <= 39)) !=
-             ((play[cardMovePos[0] - 1][cardMovePos[1]] >= 1 && play[cardMovePos[0] - 1][cardMovePos[1]] <= 13) ||
-              (play[cardMovePos[0] - 1][cardMovePos[1]] >= 27 && play[cardMovePos[0] - 1][cardMovePos[1]] <= 39))) &&
+    } else if (play[cardPos[0]][cardPos[1]] - 1 % 13 == play[cardMovePos[0] - 1][cardMovePos[1]] - 1 % 13 - 1 &&
+               (((play[cardPos[0]][cardPos[1]] >= 1 && play[cardPos[0]][cardPos[1]] <= 13) ||
+                 (play[cardPos[0]][cardPos[1]] >= 27 && play[cardPos[0]][cardPos[1]] <= 39)) !=
+                ((play[cardMovePos[0] - 1][cardMovePos[1]] >= 1 && play[cardMovePos[0] - 1][cardMovePos[1]] <= 13) ||
+                 (play[cardMovePos[0] - 1][cardMovePos[1]] >= 27 && play[cardMovePos[0] - 1][cardMovePos[1]] <= 39))) &&
                play[cardPos[0]][cardPos[1]] % 13 != 0) {
         cardMove(play, hidden, cardPos, cardMovePos);
         printf("Valid move.\n");
