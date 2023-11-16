c4bee6ed9ff9b18521bd602417799c8a0eb72c01
ahobel1995 <ahobel1995@gmail.com>
2023-11-16 15:32:00 -0700

    Updating Files

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "gameLogic.h"
 
 int main() {
-    int yesNo, game = 0, inc = 0;
+    int yesNo, moveYesNo, game = 0, inc = 0;
     int deck[53], hidden[31][7], play[31][7];
     int dropDeck[4];
     int playerCardChoice[2], playerMoveChoice[2];
@@ -35,18 +35,17 @@ int main() {
         while (game == 0) {
             frameGen(play, hidden, deck, dropDeck, inc);
             printf("Would you like to move a card? (y=1, n=0) ");
-            while (scanf("%d", &yesNo) != 1 || (yesNo != 1 && yesNo != 0)) {
+            while (scanf("%d", &moveYesNo) != 1 || (moveYesNo != 1 && moveYesNo != 0)) {
                 while (getchar() != '\n');
                 printf("Error. Please input a 1 or 0: ");
             }
-            if (yesNo == 0) {
+            if (moveYesNo == 0) {
                 break;
             } else {
                 playerMoveDecision(play, playerCardChoice, playerMoveChoice);
                 cardMoveEval(play, hidden, playerCardChoice, playerMoveChoice);
             }
         }
-
         printf("Would you like to play again? (y=1,n=0) ");
     } while (yesNo == 1);
     system("clear");
