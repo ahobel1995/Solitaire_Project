b6e3e2b6164d50be68230eb19bf9a3bf626f0b3e
ahobel1995 <ahobel1995@gmail.com>
2023-11-16 13:44:11 -0700

    Updating Files

--- gameLogic.c
+++ gameLogic.c
@@ -19,8 +19,13 @@ void cardMoveEval(int play[19][7], int hidden[19][7], int cardPos[2], int cardMo
     if (play[cardPos[0]][cardPos[1]] == 0) {
         printf("Error. Please choose a card.\n");
         return;
+        // The following check is to ensure the card is one lower, opposite suit, and not a king for a valid move.
     } else if (play[cardPos[0]][cardPos[1]] % 13 == play[cardMovePos[0] - 1][cardMovePos[1]] % 13 + 1 &&
-               play[cardPos[0]][cardPos[1]] / 13 % 2 != play[cardMovePos[0] - 1][cardMovePos[1]] / 13 % 2) {
+               play[cardPos[0]][cardPos[1]] / 13 % 2 != play[cardMovePos[0] - 1][cardMovePos[1]] / 13 % 2 &&
+               play[cardPos[0]][cardPos[1]] % 13 != 0 ) {
+        cardMove(play, hidden, cardPos, cardMovePos);
+        // This check is to check for a King and then ensures the move position is the top row.
+    } else if (play[cardPos[0]][cardPos[1]] % 13 == 0 && cardMovePos[0] == 0) {
         cardMove(play, hidden, cardPos, cardMovePos);
     } else {
         printf("Error. Please choose a valid move position.\n");
