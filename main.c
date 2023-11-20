efd0d52c64891c8cf4d21bfc8ac81be61c3e81f3
ahobel1995 <ahobel1995@gmail.com>
2023-11-19 23:34:44 -0700

    Updating Files

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "gameLogic.h"
 
 int main() {
-    int yesNo, moveYesNo, game = 0, inc = 1;
+    int yesNo, moveYesNo, game = 0, inc = 0;
     int deck[53], hidden[21][7], play[21][7];
     int dropDeck[4];
     int playerCardChoice[2], playerMoveChoice[2], i, j;
@@ -26,7 +26,7 @@ int main() {
         hiddenGen(hidden);
         playGen(play, deck);
         dropDeckReset(dropDeck);
-        int drawDeck = deck[30];
+        int drawDeck = deck[inc + 30];
 
         // GAME GOES HERE:
         while (game == 0) {
