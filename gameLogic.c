2a29bbbd7c5fef626744f6155a75f5e8a9601a40
ahobel1995 <ahobel1995@gmail.com>
2023-11-19 21:49:12 -0700

    Updating Files

--- gameLogic.c
+++ gameLogic.c
@@ -56,7 +56,7 @@ void cardMoveEval(int play[19][7], int hidden[19][7], int cardPos[2], int cardMo
 
         // Drop Deck evaluation
     } else if (cardMovePos[1] == 7) {
-        if (play[cardPos[0]][cardPos[1] + 1] > 0) {
+        if (play[cardPos[0]][cardPos[1] + 2] > 0) {
             printf("Invalid Selection. Your card must be singular and not in a stack for the drop deck.\n");
             sleep(3);
             return;
