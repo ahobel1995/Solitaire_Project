d89705f76022bd635aefed16d63c0ea157dd3a3c
ahobel1995 <ahobel1995@gmail.com>
2023-11-21 15:16:59 -0700

    Updating Files

--- main.c
+++ main.c
@@ -4,6 +4,12 @@
 #include "solFunc.h"
 #include "gameLogic.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "solFunc.h"
+#include "gameLogic.h"
+
 int main() {
     int yesNo, moveYesNo, game = 0, draw = 29;
     int deck[53], hidden[19][7], play[22][7], dropDeck[4];
@@ -25,14 +31,14 @@ int main() {
         game = 0; // Used to start a new game.
 
         // RESET FUNCTIONS:
-        deckResetShuffle(deck, *drawptr); // Assuming deckResetShuffle() is now void
+        deckResetShuffle(deck, drawptr); // Assuming deckResetShuffle() is now void
         hiddenGen(hidden);
         playGen(play, deck);
         dropDeckReset(dropDeck);
 
         // GAME GOES HERE:
         while (game == 0) {
-            frameGen(play, hidden, deck, dropDeck, *drawptr);
+            frameGen(play, hidden, deck, dropDeck, drawptr);
             printf("Would you like to move a card? (y=1, n=0) ");
             while (scanf("%d", &moveYesNo) != 1 || (moveYesNo != 1 && moveYesNo != 0)) {
                 while (getchar() != '\n');
@@ -47,21 +53,13 @@ int main() {
                 cardMoveEval(play, hidden, playerCardChoice, playerMoveChoice, dropDeck, deck, drawptr, moveptr);
             }
             if (dropDeck[0] == 13 && dropDeck[1] == 26 && dropDeck[2] == 39 && dropDeck[3] == 52) {
-                frameGen(play, hidden, deck, dropDeck, *drawptr);
-                printf("\nCongratulations! You win! \nYou made %d moves to win!\n\n", *moveptr);
+                frameGen(play, hidden, deck, dropDeck, drawptr);
+                printf("\nCongratulations! You win! \nYou made %d moves.\n", *moveptr);
                 game = 1;
             }
         }
-        printf("Would you like to play again?  (y=1, n=0) ");
-    } while (yesNo == 1);
-    system("clear");
-    switch (game) {
-        case 0:
-            mikeDisappointed();
-            break;
-        case 1:
-            gigaChad();
-            break;
-    }
+    } while (yesNo != 0);
+
+    printf("Thank you for playing!\n");
     return 0;
 }
