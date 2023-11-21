863098dd97a4a98cfacb69d74c52c998a9a5c9f1
ahobel1995 <ahobel1995@gmail.com>
2023-11-21 15:43:04 -0700

    Updating Files

--- solFunc.c
+++ solFunc.c
@@ -66,22 +66,22 @@ void frameGen(int play[21][7], int hidden[21][7], int deck[53], int dropDeck[4],
     system("clear");
 
     // Print Draw Deck:
-    printf("      ");
+    printf("       ");
     printf("\033[0;30m\033[48;5;240m");  // Black text, grey background
     printf("Top of Draw Deck:   ");
     numCardPrint(deck[*draw]);
     printf("\033[0;30m\033[48;5;240m");  // Black text, grey background
-    printf("      ");
+    printf("     ");
     printf("\033[0m"); // Reset text to default
 
     // Print Drop Decks:
-    printf("\n        ");
+    printf("\n       ");
     printf("\033[0;30m\033[48;5;240m"); // Black text, grey background
     printf("Drop Decks: ");
     for(i=0; i<4; i++){
         numCardPrint(dropDeck[i]);
     }
-    printf("                 \n");
+    printf("                  \n");
     // For style to match length of horizontal lines:
     printf("       ");
     printf("\033[0;30m\033[48;5;240m"); // Black text, grey background
