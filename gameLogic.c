664ad392ce06b2b307e70ab45dcff2beb8f18ee7
ahobel1995 <ahobel1995@gmail.com>
2023-11-28 21:40:58 -0700

    Updating Files

--- gameLogic.c
+++ gameLogic.c
@@ -307,7 +307,7 @@ void playerMoveDecision(int play[22][7], int hidden[19][7], int dropDeck[4], int
             else if (decisionArray[2] == '\n' && decisionArray[0] >= '1' && decisionArray[0] <= '7') {
                 // Iterate through the column to find the topmost card
                 for (i = 19; i > 0; i--) {
-                    if (play[i][playerCardChoice[1]] != 0) {
+                    if (play[i][playerCardChoice[1]-1] != 0) {
                         playerCardChoice[0] = i; // Set the row of the topmost card to move
                         correct = 1;
                         break;
