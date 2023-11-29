61f54c9ca458465c209d3e174d70ac06d864365f
ahobel1995 <ahobel1995@gmail.com>
2023-11-28 21:17:55 -0700

    Updating Files

--- gameLogic.c
+++ gameLogic.c
@@ -307,7 +307,7 @@ void playerMoveDecision(int play[22][7], int hidden[19][7], int dropDeck[4], int
                 for (i = 19; i > 0; i--) {
                     playerCardChoice[0] = i - 1;
                     if (play[playerCardChoice[0]][playerCardChoice[1]] != 0) {
-                        playerCardChoice[0]--;
+                        playerCardChoice[0] = i + 1;
                         correct = 1;
                         break;
                     }
@@ -368,4 +368,5 @@ void playerMoveDecision(int play[22][7], int hidden[19][7], int dropDeck[4], int
     }
     printf("Player Card Choice: %d, %d\n", playerCardChoice[0], playerCardChoice[1]);
     printf("Player Move Choice: %d, %d\n", playerMoveChoice[0], playerMoveChoice[1]);
+    sleep(2);
 }
